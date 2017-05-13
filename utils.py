17bdad9afe78468c18fb7860fc46b44a576b07da
Jesper Segeblad <jsegeblad@gmail.com>
2017-05-13 15:30:53 +0200

    update plotting

--- utils.py
+++ utils.py
@@ -156,7 +156,7 @@ def write_misclassifications(data, outf):
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           title='',
-                          cmap=plt.cm.Blues):
+                          cmap=plt.cm.gist_yarg):
     """
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
@@ -165,11 +165,12 @@ def plot_confusion_matrix(cm, classes,
     plt.title(title)
     tick_marks = np.arange(len(classes))
     plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
+    plt.yticks(tick_marks, classes, rotation=45)
 
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         print("Normalized confusion matrix")
+        cm = np.around(cm, decimals=4) * 100
     else:
         print('Confusion matrix, without normalization')
 
@@ -178,11 +179,11 @@ def plot_confusion_matrix(cm, classes,
     thresh = cm.max() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         plt.text(j, i, cm[i, j],
-                 horizontalalignment="center",
+                 horizontalalignment="center", fontsize=13,
                  color="white" if cm[i, j] > thresh else "black")
 
     plt.tight_layout()
-    plt.ylabel('True label')
+    # plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.show()
 
