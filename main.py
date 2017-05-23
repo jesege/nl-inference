d11bb2092cc5d9d297bcd7adecd11dcb9788d1c4
Jesper Segeblad <jsegeblad@gmail.com>
2017-05-23 16:35:14 +0200

    change the testing code...

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ for epoch in range(1, args.epochs + 1):
                 "Lowered learning rate to %.2e after %d iterations" %
                 (new_lr, iteration))
 
-        if iteration % 50 == 0:
+        if iteration % 5000 == 0:
             dev_loss, correct_dev = model.validate(network, dev_loader)
             dev_accuracy = correct_dev / len(dev_loader.dataset)
             training_logger.info(
